682367ad71fcac614f2135bf6e53dc2055ebda90
RamanDudoits <didoitsrv@gmail.com>
2021-03-12 12:49:45 +0300

    buy one click

--- local/components/mycomponent/buy.oneclick/templates/.default/template.php
+++ local/components/mycomponent/buy.oneclick/templates/.default/template.php
@@ -1,7 +1,7 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <button class="btn-one-click">Купить в один клик</button>
-<div class="one-click" ">
+<div class="one-click" style="display: none;" >
     <input class="phone" placeholder="Номер телефона">
     <a href="#" class="buy-one-click" data-id="<?=$arParams["PRODUCT_ID"]?>"
             data-params-object="<?=$arParams["ACTION"]?>">Оформить заказ</a>
